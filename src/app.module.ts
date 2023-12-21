4c853aac220a3d2885b182586a37b8491946d57f
RillSvai <kirilkvas07@gmail.com>
2023-12-21 15:17:14 +0200

    Setup and defined user schema

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.dev.env',
+    }),
+  ],
+})
+export class AppModule {}
